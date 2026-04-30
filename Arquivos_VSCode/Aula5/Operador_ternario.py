d88a73168c7e14ed3f39b0c4f97dae4edd2b48a7
Josevitor1204 <jose.v.rodrigues11@aluno.senai.br>
2026-04-30 12:12:07 -0300

    correçao de atividades e novos codigos

---
+++ Arquivos_VSCode/Aula5/Operador_ternario.py
@@ -0,0 +1,5 @@
+num1 = float(input("Digite o primeiro numero: "))
+num2 = float(input("Digite o segundo numero: "))
+
+result = "num1 é maior" if num1 > num2 else "num1 é menor"
+print(result)
--- Arquivos_VSCode/Aula5/app.py
+++ Arquivos_VSCode/Aula5/app.py
@@ -30,4 +30,51 @@ print("A média dos números são: ", media)
 
 #Questão5
 
+'''
+nome = str(input("Digite seu nome: "))
+print (type(nome))'''
 
+
+"""
+#Questão6
+
+lista_principal=[]
+dobro=[]"""
+
+
+'''
+#Questão7
+
+num1 = float(input("Digite o primeiro numero: "))
+num2 = float(input("Digite o segundo numero: "))
+
+result = num1 > num2
+print(result)'''
+
+
+'''
+#Questão8
+
+nome1 = input("Digite o primeiro nome completo: ")
+nome2 = input("Digite o segundo nome completo: ")
+
+# separando o nome do sobrenome
+parte1 = nome1.split()
+parte2 = nome2.split()
+
+# pegar o primeiro nome e sobrenome
+primeiro_nome1 = parte1[0]
+sobrenome1 = parte1[-1]
+
+primeiro_nome2 = parte2[0]
+sobrenome2 = parte2[-1]
+
+novo_nome1 = primeiro_nome1 + " " + sobrenome2
+novo_nome2 = primeiro_nome2 + " " + sobrenome1
+
+print(novo_nome1)
+print(novo_nome2)'''
+
+'''
+
+'''
