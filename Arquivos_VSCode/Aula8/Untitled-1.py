2fd62858171bb0b6ce10ca0eb64d48ce89459428
Josevitor1204 <jose.v.rodrigues11@aluno.senai.br>
2026-06-09 12:14:13 -0300

    Aula 9

---
+++ Arquivos_VSCode/Aula8/Untitled-1.py
@@ -0,0 +1,3 @@
+idade = int(input("Digite sua idade: "))
+if idade > 17:
+    print("Você está na maioridade!")
---
+++ Arquivos_VSCode/Aula9/app.py
@@ -0,0 +1,162 @@
+'''
+Sistema de veículos - -em json
+'''
+
+import os
+import time
+import json
+
+ARQUIVO = 'carros.json'
+
+# carrega o arquivo
+
+if os.path.exists(ARQUIVO):
+    with open(ARQUIVO, 'r', encoding='utf-8') as arquivo:
+        carros = json.load(arquivo)
+else:
+    carro = []
+
+
+# descobrir o ID
+if carro:
+    proximo_id = max(carros['id'] for carro in carros) +1
+else:
+    proximo_id = 1
+os.system('cls')
+
+while True:
+    print('\n============= Sistema de Carros 🚗 =============')
+    print('1 - Cadastrar Carros')
+    print('2 - Listar Carros')
+    print('3 - Atualizar Carros')
+    print('4 - Deletar Carros')
+    print('0 - Sair')
+
+    opcao = input('Escolha uma opção: ')
+
+    os.system('cls')
+
+    #create
+    if opcao == '1':
+        modelo = input('Digite o modelo do carro: ').title()
+        preco = float(input('Digite o preço: '))
+        marca = input('Digite a marca: ').title()
+
+        carro = {
+            "id"         : proximo_id,
+            "modelo"    : modelo,
+            "preco"      : preco,
+            "marca"      : marca
+        }
+
+        carros.append(carro)
+
+        with open(ARQUIVO, 'w', encoding='utf-8') as arquivo:
+            json.dump(carros,arquivo,indent=4, ensure_ascii=False)
+            
+        proximo_id+=1
+
+        print('Carro cadastrado com sucesso! ✅')
+
+    elif opcao =="2":
+        os.system('cls')
+
+        if not carros:
+            print ('Nenhum veiculo encontrado!')
+
+        else:
+            print('\n📋 Lista de veiculos')
+
+            for carro in carros:
+                print(
+                    f'ID:{carro['ID']}|'
+                    f'Modelo:{carro['modelo']}|'
+                    f'preço:{carro['preco']}|'
+                    f'Marca:{carro['marca']}|'
+                )
+    elif opcao == '3':
+        os.system('cls')
+
+        if not carros:
+            print('Nenhum carro encontrado!')
+            continue
+
+        print('\n📋 Lista de carros')
+        for carro in carros:
+            print(
+                    f'ID:{carro['id']}|'
+                    f'Modelo:{carro['modelo']}|'
+                    f'preço:{carro['preco']}|'
+                    f'Marca:{carro['marca']}|'
+                )
+        id_busca = int(input('Digite o ID do carro para buscar: '))
+
+        encontrado = False
+
+        for carro in carros:
+            if carro ['id'] == id_busca:
+                novo_modelo = input('Digite o novo modelo: ').title()
+                novo_preco = float(input('Digite o novo preço: ').replace(',', '.'))
+                nova_marca = input('Digite a nova marca: ').title()
+
+                with open(ARQUIVO, 'W', encoding='utf-8')as arquivo:
+                    json.dump(
+                        carros,
+                        arquivo,
+                        indent=4,
+                        ensure_ascii=False
+                    )
+                print ('Carro atualizado com sucesso!')
+                encontrado= True
+                break
+    elif opcao == '4':
+        os.system('cls')
+
+        if not carros:
+            print('Nenhum carro encontrado!')
+            continue
+
+        print('\n📋 Lista de carros')
+        for carro in carros:
+            print(
+                    f'ID:{carro['id']}|'
+                    f'Modelo:{carro['modelo']}|'
+                    f'preço:{carro['preco']}|'
+                    f'Marca:{carro['marca']}|'
+                )
+        id_buscar = int(input('Digite o ID do carro para deletar: '))
+
+        encontrado = False
+        
+        for carro in carros:
+            if carro ['id'] == id_busca:
+                novo_modelo = input('Digite o novo modelo: ').title()
+                novo_preco = float(input('Digite o novo preço: ').replace(',', '.'))
+                nova_marca = input('Digite a nova marca: ').title()
+
+                with open(ARQUIVO, 'W', encoding='utf-8')as arquivo:
+                    json.dump(
+                        carros,
+                        arquivo,
+                        indent=4,
+                        ensure_ascii=False
+                    )
+                print ('Carro deletado com sucesso!')
+                encontrado= True
+                break
+    elif opcao == '0':
+        total = 20
+        barra =""
+        print('Saindo do Sistema...')
+        for i in range(1, total +1):
+            barra +="🟩"
+            porcentagem = int((i / total *100))
+            vazio = "-" * (total -1)
+            print(f'\r[{barra}] {porcentagem}%', end="")
+            time.sleep(0.2)
+        break
+
+
+
+
+        
